870cbe314fd7f38e9dbbc77029cb1a64640a2fb7
AnishNepal <anishnepal62@gmail.com>
2021-01-10 08:53:30 +0545

    Initial Commit

---
+++ app/src/main/java/com/anishapp/week6assignment1/Dashboard.kt
@@ -0,0 +1,11 @@
+package com.anishapp.week6assignment1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
