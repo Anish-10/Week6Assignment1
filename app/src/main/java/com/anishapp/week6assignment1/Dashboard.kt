d4bb83177c42f34d4643d5ea9be85c651fae9a1d
AnishNepal <anishnepal62@gmail.com>
2021-01-12 20:17:35 +0545

    Third Commit

--- app/src/main/java/com/anishapp/week6assignment1/Dashboard.kt
+++ app/src/main/java/com/anishapp/week6assignment1/Dashboard.kt
@@ -1,11 +1,54 @@
 package com.anishapp.week6assignment1
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.anishapp.week6assignment1.fragments.AboutUs
+import com.anishapp.week6assignment1.fragments.AddStudent
+import com.anishapp.week6assignment1.fragments.Home
 
 class Dashboard : AppCompatActivity() {
+    private lateinit var RecyclerView : RecyclerView
+    private lateinit var Home : ImageView
+    private lateinit var AddStudent : ImageView
+    private lateinit var AboutUs : ImageView
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        Home = findViewById(R.id.Home)
+        AddStudent = findViewById(R.id.AddStudent)
+        AboutUs = findViewById(R.id.AboutUs)
+
+        Home.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.LinearContainer, Home()
+                )
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        AddStudent.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.LinearContainer, AddStudent())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        AboutUs.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.LinearContainer, AboutUs())
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
--- app/src/main/java/com/anishapp/week6assignment1/fragments/AddStudent.kt
+++ app/src/main/java/com/anishapp/week6assignment1/fragments/AddStudent.kt
@@ -5,56 +5,81 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.anishapp.week6assignment1.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddStudent.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddStudent : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var Fullname : EditText
+    private lateinit var Age : EditText
+    private lateinit var Address : EditText
+    private lateinit var Male : RadioButton
+    private lateinit var Female : RadioButton
+    private lateinit var Others : RadioButton
+    private lateinit var Save : Button
+
+    private var gender = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_student, container, false)
+        val view = inflater.inflate(R.layout.fragment_add_student, container, false)
+        Fullname = view.findViewById(R.id.Fullname)
+        Age = view.findViewById(R.id.Age)
+        Address = view.findViewById(R.id.Address)
+        Male = view.findViewById(R.id.Male)
+        Female = view.findViewById(R.id.Female)
+        Others = view.findViewById(R.id.Others)
+        Save = view.findViewById(R.id.Save)
+
+        Male.setOnClickListener {
+            gender = Male.text.toString()
+        }
+
+        Female.setOnClickListener {
+            gender = Female.text.toString()
+        }
+
+        Others.setOnClickListener {
+            gender = Others.text.toString()
+        }
+
+        Save.setOnClickListener {
+            checkNull()
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddStudent.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddStudent().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun checkNull()
+    {
+        if(Fullname.text.toString() == "")
+        {
+            Fullname.error = "Please enter your full name"
+            Fullname.requestFocus()
+        }
+        else if(Age.text.toString() == "")
+        {
+            Age.error = "Please enter your age"
+            Age.requestFocus()
+        }
+        else if(Address.text.toString() == "")
+        {
+            Address.error = "Please enter your address"
+            Address.requestFocus()
+        }
+        else if(gender == "")
+        {
+            Toast.makeText(context,"Select your gender",Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            Toast.makeText(context,"Details Saved.",Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
